df6a3c428bcdf2ef1ddc0ae9fe2ef7b73f2318ee
yang <1351599172@qq.com>
2019-06-16 14:56:40 +0800

    add validate.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/silverswords/clouds
+
+go 1.12
+
+require (
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2
+)
---
+++ validate.go
@@ -0,0 +1,46 @@
+package core
+
+import (
+	"reflect"
+	"sync"
+
+	"gopkg.in/go-playground/validator.v8"
+)
+
+var (
+	// Validator -
+	Validator = &defaultValidator{}
+)
+
+type defaultValidator struct {
+	once     sync.Once
+	validate *validator.Validate
+}
+
+// Validate -
+func Validate(obj interface{}) error {
+	return Validator.ValidateStruct(obj)
+}
+
+// ValidateStruct receives any kind of type, but only performed struct or pointer to struct type.
+func (v *defaultValidator) ValidateStruct(obj interface{}) error {
+	value := reflect.ValueOf(obj)
+	valueType := value.Kind()
+	if valueType == reflect.Ptr {
+		valueType = value.Elem().Kind()
+	}
+	if valueType == reflect.Struct {
+		v.lazyinit()
+		if err := v.validate.Struct(obj); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (v *defaultValidator) lazyinit() {
+	v.once.Do(func() {
+		config := &validator.Config{TagName: "zeit"}
+		v.validate = validator.New(config)
+	})
+}
