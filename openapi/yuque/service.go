eee155d7aecc4aabedb01bc378d2283dacf21902
yang <1351599172@qq.com>
2019-06-18 17:52:05 +0800

    Update service.go

--- openapi/yuque/service.go
+++ openapi/yuque/service.go
@@ -1,6 +1,7 @@
 package yuque
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -19,11 +20,12 @@ func NewService(token string) *Service {
 }
 
 // List -
-func (s *Service) List(repoid string) (string, error) {
+func (s *Service) List(repoid string) (interface{}, error) {
+	var List BookDetail
 	url := fmt.Sprintf(ListURL, repoid)
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	request.Header.Add("X-Auth-Token", s.Token)
@@ -31,24 +33,30 @@ func (s *Service) List(repoid string) (string, error) {
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(body), nil
+	err = json.Unmarshal(body, &List)
+	if err != nil {
+		return nil, err
+	}
+
+	return List, nil
 }
 
 // Details -
-func (s *Service) Details(repoid, id string) (string, error) {
+func (s *Service) Details(repoid, id string) (interface{}, error) {
+	var Details DocDetail
 	url := fmt.Sprintf(DetailURL, repoid, id)
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	request.Header.Add("X-Auth-Token", s.Token)
@@ -56,24 +64,31 @@ func (s *Service) Details(repoid, id string) (string, error) {
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(body), nil
+	err = json.Unmarshal(body, &Details)
+	if err != nil {
+		return nil, err
+	}
+
+	return Details, nil
 }
 
 // Repo -
-func (s *Service) Repo(groupid string) (string, error) {
+func (s *Service) Repo(groupid string) (interface{}, error) {
+	var Repo Book
+
 	url := fmt.Sprintf(RepoURL, groupid)
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	request.Header.Add("X-Auth-Token", s.Token)
@@ -81,14 +96,19 @@ func (s *Service) Repo(groupid string) (string, error) {
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(body), nil
+	err = json.Unmarshal(body, &Repo)
+	if err != nil {
+		return nil, err
+	}
+
+	return Repo, nil
 }
