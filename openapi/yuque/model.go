dee9f701c7e58b0ae4e227fb31916154d8a71c31
yang <1351599172@qq.com>
2019-06-19 01:05:00 +0800

    Update model.go

--- openapi/yuque/model.go
+++ openapi/yuque/model.go
@@ -39,22 +39,22 @@ type DocDetail struct {
 			CreatedAt        time.Time `json:"created_at"`
 			Namespace        string    `json:"namespace"`
 			User             struct {
-				ID               int         `json:"id"`
-				Type             string      `json:"type"`
-				Login            string      `json:"login"`
-				Name             string      `json:"name"`
-				Description      interface{} `json:"description"`
-				AvatarURL        string      `json:"avatar_url"`
-				LargeAvatarURL   string      `json:"large_avatar_url"`
-				MediumAvatarURL  string      `json:"medium_avatar_url"`
-				SmallAvatarURL   string      `json:"small_avatar_url"`
-				BooksCount       int         `json:"books_count"`
-				PublicBooksCount int         `json:"public_books_count"`
-				FollowersCount   int         `json:"followers_count"`
-				FollowingCount   int         `json:"following_count"`
-				CreatedAt        time.Time   `json:"created_at"`
-				UpdatedAt        time.Time   `json:"updated_at"`
-				Serializer       string      `json:"_serializer"`
+				ID               int       `json:"id"`
+				Type             string    `json:"type"`
+				Login            string    `json:"login"`
+				Name             string    `json:"name"`
+				Description      string    `json:"description"`
+				AvatarURL        string    `json:"avatar_url"`
+				LargeAvatarURL   string    `json:"large_avatar_url"`
+				MediumAvatarURL  string    `json:"medium_avatar_url"`
+				SmallAvatarURL   string    `json:"small_avatar_url"`
+				BooksCount       int       `json:"books_count"`
+				PublicBooksCount int       `json:"public_books_count"`
+				FollowersCount   int       `json:"followers_count"`
+				FollowingCount   int       `json:"following_count"`
+				CreatedAt        time.Time `json:"created_at"`
+				UpdatedAt        time.Time `json:"updated_at"`
+				Serializer       string    `json:"_serializer"`
 			} `json:"user"`
 			Serializer string `json:"_serializer"`
 		} `json:"book"`
@@ -77,53 +77,53 @@ type DocDetail struct {
 			UpdatedAt        time.Time `json:"updated_at"`
 			Serializer       string    `json:"_serializer"`
 		} `json:"creator"`
-		Format            string      `json:"format"`
-		Body              string      `json:"body"`
-		BodyDraft         string      `json:"body_draft"`
-		BodyHTML          string      `json:"body_html"`
-		BodyLake          string      `json:"body_lake"`
-		Public            int         `json:"public"`
-		Status            int         `json:"status"`
-		LikesCount        int         `json:"likes_count"`
-		CommentsCount     int         `json:"comments_count"`
-		ContentUpdatedAt  time.Time   `json:"content_updated_at"`
-		DeletedAt         interface{} `json:"deleted_at"`
-		CreatedAt         time.Time   `json:"created_at"`
-		UpdatedAt         time.Time   `json:"updated_at"`
-		PublishedAt       time.Time   `json:"published_at"`
-		FirstPublishedAt  time.Time   `json:"first_published_at"`
-		WordCount         int         `json:"word_count"`
-		Cover             interface{} `json:"cover"`
-		Description       string      `json:"description"`
-		CustomDescription interface{} `json:"custom_description"`
-		Serializer        string      `json:"_serializer"`
+		Format            string    `json:"format"`
+		Body              string    `json:"body"`
+		BodyDraft         string    `json:"body_draft"`
+		BodyHTML          string    `json:"body_html"`
+		BodyLake          string    `json:"body_lake"`
+		Public            int       `json:"public"`
+		Status            int       `json:"status"`
+		LikesCount        int       `json:"likes_count"`
+		CommentsCount     int       `json:"comments_count"`
+		ContentUpdatedAt  time.Time `json:"content_updated_at"`
+		DeletedAt         time.Time `json:"deleted_at"`
+		CreatedAt         time.Time `json:"created_at"`
+		UpdatedAt         time.Time `json:"updated_at"`
+		PublishedAt       time.Time `json:"published_at"`
+		FirstPublishedAt  time.Time `json:"first_published_at"`
+		WordCount         int       `json:"word_count"`
+		Cover             string    `json:"cover"`
+		Description       string    `json:"description"`
+		CustomDescription string    `json:"custom_description"`
+		Serializer        string    `json:"_serializer"`
 	} `json:"data"`
 }
 
 // BookDetail -
 type BookDetail struct {
 	Data []struct {
-		ID                int         `json:"id"`
-		Slug              string      `json:"slug"`
-		Title             string      `json:"title"`
-		Description       string      `json:"description"`
-		UserID            int         `json:"user_id"`
-		BookID            int         `json:"book_id"`
-		Format            string      `json:"format"`
-		Public            int         `json:"public"`
-		Status            int         `json:"status"`
-		LikesCount        int         `json:"likes_count"`
-		CommentsCount     int         `json:"comments_count"`
-		ContentUpdatedAt  time.Time   `json:"content_updated_at"`
-		CreatedAt         time.Time   `json:"created_at"`
-		UpdatedAt         time.Time   `json:"updated_at"`
-		PublishedAt       time.Time   `json:"published_at"`
-		FirstPublishedAt  time.Time   `json:"first_published_at"`
-		DraftVersion      int         `json:"draft_version"`
-		LastEditorID      int         `json:"last_editor_id"`
-		WordCount         int         `json:"word_count"`
-		Cover             interface{} `json:"cover"`
-		CustomDescription interface{} `json:"custom_description"`
+		ID                int       `json:"id"`
+		Slug              string    `json:"slug"`
+		Title             string    `json:"title"`
+		Description       string    `json:"description"`
+		UserID            int       `json:"user_id"`
+		BookID            int       `json:"book_id"`
+		Format            string    `json:"format"`
+		Public            int       `json:"public"`
+		Status            int       `json:"status"`
+		LikesCount        int       `json:"likes_count"`
+		CommentsCount     int       `json:"comments_count"`
+		ContentUpdatedAt  time.Time `json:"content_updated_at"`
+		CreatedAt         time.Time `json:"created_at"`
+		UpdatedAt         time.Time `json:"updated_at"`
+		PublishedAt       time.Time `json:"published_at"`
+		FirstPublishedAt  time.Time `json:"first_published_at"`
+		DraftVersion      int       `json:"draft_version"`
+		LastEditorID      int       `json:"last_editor_id"`
+		WordCount         int       `json:"word_count"`
+		Cover             string    `json:"cover"`
+		CustomDescription string    `json:"custom_description"`
 		LastEditor        struct {
 			ID              int       `json:"id"`
 			Type            string    `json:"type"`
@@ -164,22 +164,22 @@ type Book struct {
 		CreatedAt        time.Time `json:"created_at"`
 		Namespace        string    `json:"namespace"`
 		User             struct {
-			ID               int         `json:"id"`
-			Type             string      `json:"type"`
-			Login            string      `json:"login"`
-			Name             string      `json:"name"`
-			Description      interface{} `json:"description"`
-			AvatarURL        string      `json:"avatar_url"`
-			LargeAvatarURL   string      `json:"large_avatar_url"`
-			MediumAvatarURL  string      `json:"medium_avatar_url"`
-			SmallAvatarURL   string      `json:"small_avatar_url"`
-			BooksCount       int         `json:"books_count"`
-			PublicBooksCount int         `json:"public_books_count"`
-			FollowersCount   int         `json:"followers_count"`
-			FollowingCount   int         `json:"following_count"`
-			CreatedAt        time.Time   `json:"created_at"`
-			UpdatedAt        time.Time   `json:"updated_at"`
-			Serializer       string      `json:"_serializer"`
+			ID               int       `json:"id"`
+			Type             string    `json:"type"`
+			Login            string    `json:"login"`
+			Name             string    `json:"name"`
+			Description      string    `json:"description"`
+			AvatarURL        string    `json:"avatar_url"`
+			LargeAvatarURL   string    `json:"large_avatar_url"`
+			MediumAvatarURL  string    `json:"medium_avatar_url"`
+			SmallAvatarURL   string    `json:"small_avatar_url"`
+			BooksCount       int       `json:"books_count"`
+			PublicBooksCount int       `json:"public_books_count"`
+			FollowersCount   int       `json:"followers_count"`
+			FollowingCount   int       `json:"following_count"`
+			CreatedAt        time.Time `json:"created_at"`
+			UpdatedAt        time.Time `json:"updated_at"`
+			Serializer       string    `json:"_serializer"`
 		} `json:"user"`
 		Serializer string `json:"_serializer"`
 	} `json:"data"`
