ac683e38bc2df6f1e60092edecea8c8e9af0b0f8
yang <1351599172@qq.com>
2019-06-19 10:30:45 +0800

    Update model.go

--- openapi/yuque/model.go
+++ openapi/yuque/model.go
@@ -103,27 +103,27 @@ type DocDetail struct {
 // BookDetail -
 type BookDetail struct {
 	Data []struct {
-		ID                int       `json:"id"`
-		Slug              string    `json:"slug"`
-		Title             string    `json:"title"`
-		Description       string    `json:"description"`
-		UserID            int       `json:"user_id"`
-		BookID            int       `json:"book_id"`
-		Format            string    `json:"format"`
-		Public            int       `json:"public"`
-		Status            int       `json:"status"`
-		LikesCount        int       `json:"likes_count"`
-		CommentsCount     int       `json:"comments_count"`
-		ContentUpdatedAt  time.Time `json:"content_updated_at"`
-		CreatedAt         time.Time `json:"created_at"`
-		UpdatedAt         time.Time `json:"updated_at"`
-		PublishedAt       time.Time `json:"published_at"`
-		FirstPublishedAt  time.Time `json:"first_published_at"`
-		DraftVersion      int       `json:"draft_version"`
-		LastEditorID      int       `json:"last_editor_id"`
-		WordCount         int       `json:"word_count"`
-		Cover             string    `json:"cover"`
-		CustomDescription string    `json:"custom_description"`
+		ID                int         `json:"id"`
+		Slug              string      `json:"slug"`
+		Title             string      `json:"title"`
+		Description       string      `json:"description"`
+		UserID            int         `json:"user_id"`
+		BookID            int         `json:"book_id"`
+		Format            string      `json:"format"`
+		Public            int         `json:"public"`
+		Status            int         `json:"status"`
+		LikesCount        int         `json:"likes_count"`
+		CommentsCount     int         `json:"comments_count"`
+		ContentUpdatedAt  time.Time   `json:"content_updated_at"`
+		CreatedAt         time.Time   `json:"created_at"`
+		UpdatedAt         time.Time   `json:"updated_at"`
+		PublishedAt       time.Time   `json:"published_at"`
+		FirstPublishedAt  time.Time   `json:"first_published_at"`
+		DraftVersion      int         `json:"draft_version"`
+		LastEditorID      int         `json:"last_editor_id"`
+		WordCount         int         `json:"word_count"`
+		Cover             interface{} `json:"cover"`
+		CustomDescription interface{} `json:"custom_description"`
 		LastEditor        struct {
 			ID              int       `json:"id"`
 			Type            string    `json:"type"`
