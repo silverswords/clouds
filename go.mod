3302a8a0f024ff5bf91984fbdfe3518641e940e9
yang <1351599172@qq.com>
2019-07-15 11:51:41 +0800

    add githubClient

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/silverswords/clouds
 go 1.12
 
 require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2
 )
--- openapi/github/service.go
+++ openapi/github/service.go
@@ -5,7 +5,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	gogithub "github.com/google/go-github/github"
 )
+
+// APIClient encapsulate github.Client
+type APIClient struct {
+	Client *gogithub.Client
+}
+
+// NewAPIClient creates GitHubClient
+func NewAPIClient(g *http.Client) *APIClient {
+	client := gogithub.NewClient(g)
+	return &APIClient{
+		Client: client,
+	}
+}
 
 // Service encapsulate authenticated token
 type Service struct {
